37f6e244bbcfe84acaa0b5e987f1974b209bb4e3
Alexander Torres <alexander20012@hotmail.com>
2024-11-07 22:52:20 -0400

    cambios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,58 +1,122 @@
 const jwt = require("jsonwebtoken");
 const {users} = require("../db");
+const {Op} = require("sequelize");
+const bcrypt = require("bcrypt");
 
-function login({username, password}) {
+async function login(req,res) {
     let result={};
+    let {username,password} = req.body;
     try {
         if(username !="" && password !=""){
-
+            let dtaUser= await users.findOne({
+                where: {
+                    username:{
+                        [Op.eq]: username
+                    }
+                }
+            });
+            if(dtaUser){
+                const isMatch = await bcrypt.compare(password, dtaUser.password);
+                if(isMatch){
+                    let token = jwt.sign({id: dtaUser.id}, process.env.SECRET_KEY, {
+                        expiresIn: 86400 // 24 hours
+                    });
+                    result = {
+                        success: true,
+                        message: 'Registro exitoso',
+                        data: {
+                            token: token,
+                            user: dtaUser
+                        }
+                    };
+                }else{
+                    result = {
+                        success: false,
+                        message: 'Usuario o contraseña incorrecto'
+                    };
+                }
+            }
         }else{
-
+            result = {
+                success: false,
+                message: 'Usuario o contraseña incorrecto'
+            };
         }
     } catch (error) {
         result = {
             success: false,
-            message: 'Failed to login',
-            error: error
+            message: 'Error al ejecutar la funcion',
+            error: error.message
         };
     }
-    return result;
+    return res.json(result);
 }
 
-function register({username, password}) {
+async function register(req,res) {
     let result={};
+    let {
+        username,password,
+        is_active,first_name,
+        last_name,initial_balance
+    } = req.body;
     try {
-        if(username !="" && password !=""){
-
+        if(username!="" && password!="" && is_active === true && first_name!="" && last_name!="" && initial_balance >= 0){
+            let user = await users.create({
+                username,
+                password: bcrypt.hashSync(password, 10),
+                is_active,
+                first_name,
+                last_name,
+                initial_balance
+            });
+            result = {
+                success: true,
+                message: 'Registro exitoso',
+                data: user
+            };
         }else{
-
+            result = {
+                success: false,
+                message: 'Todos los campos son obligatorios'
+            };
         }
     } catch (error) {
         result = {
             success: false,
-            message: 'Failed to login',
-            error: error
+            message: 'Error al ejecutar la funcion',
+            error: error.message
         };
     }
-    return result;
+    return res.json(result);
 }
 
-function forgotPassword({username, password}) {
+async function forgotPassword(req,res) {
     let result={};
     try {
-        if(username !="" && password !=""){
+       let {username,password} = req.body;
+       let user = await users.findOne({where:{username}});
+       if(user){
+           user.password = bcrypt.hashSync(password, 10);
+           await user.save();
+           result = {
+               success: true,
+               message: 'Contraseña actualizada correctamente'
+           };
 
-        }else{
-
-        }
+       }else{
+            result = {
+                success: false,
+                message: 'Usuario no encontrado'
+            };
+       }
     } catch (error) {
-        result = {
+         result = {
             success: false,
-            message: 'Failed to login',
-            error: error
+            message: 'Error al ejecutar la funcion',
+            error: error.message
         };
     }
-    return result;
+    return res.json(result);
 }
 
 module.exports={
