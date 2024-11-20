ed3a437cdff992c65063f61476c83a4a9d2880bf
Alexander Torres <alexander20012@hotmail.com>
2024-11-19 20:36:56 -0400

    cambios

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,11 +4,12 @@ const jwt = require('jsonwebtoken');
 
 exports.validateToken = (req, res, next) => {
     try {
-      const token = req.cookies["authorization"];
+      const token = req.headers.authorization;
       if (token == null){
         return res.status(401).json({ message: "Token not provide" });
       }else{
-        jwt.verify(token, env.SECRETKEY, (err, user) => {
+        const tokenWithoutBearer = token.split(" ")[1];
+        jwt.verify(tokenWithoutBearer, env.SECRET_KEY, (err, user) => {
           if (err){
             return res.status(403).json({ message: "Token not valid" });
           } 
