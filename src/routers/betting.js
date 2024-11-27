fce51bdb82b0844c8e64f0489764fd0a431e35f2
Alexander Torres <alexander20012@hotmail.com>
2024-11-27 00:11:25 -0400

    cambios

---
+++ src/routers/betting.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const router = Router();
+const { GetAll,GetId, Create, Update, Delete} = require("../controllers/bettingController.js");
+const {validateToken} = require("../middlewares/validateToken.js");
+
+router.get("/",validateToken,GetAll)
+router.get("/:id",validateToken,GetId)
+router.post("/create",validateToken,Create);
+router.post("/update/:id",validateToken,Update);
+router.post("/delete/:id",validateToken,Delete);
+
+module.exports = router;
