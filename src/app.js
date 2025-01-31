504ee1cc7ff8e7c0873d5a3a890d8465d1a69e6c
MaxyMarquez <maxy.marquez90@gmail.com>
2025-01-31 15:32:26 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -54,8 +54,8 @@ server.use((err, req, res, next) => {
     res.status(status).send(message);
 });
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+// http.listen(process.env.PORT || 3000, () => {
+//     console.log(`Server is running on port ${process.env.PORT || 3000}`);
+// });
 
 module.exports = http;
